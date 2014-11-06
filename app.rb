5a7a6ddb3a0bcea4ecbe243ddc3f2bcbfedb1283
lcostantini <costantinileandro1@gmail.com>
2014-11-06 20:30:07 -0300

    Change HTTP for HTTParty.

--- app.rb
+++ app.rb
@@ -1,12 +1,14 @@
 #!/usr/bin/env ruby
 require './config/application'
 require 'commander/import'
-require 'net/http'
+require 'httparty'
 require 'json'
 
 program :name, 'Todos'
 program :version, '0.0.1'
 program :description, 'A console application to keep tracking of your todo lists'
+
+default_command :todo
 
 command :new do |c|
   c.syntax = 'new [options]'
@@ -15,33 +17,26 @@ command :new do |c|
   c.action do |args, options|
     name = args.join(' ') || ask("Name: ")
     description = options.d
-    uri = URI.parse('http://localhost:9393')
-    http = Net::HTTP.new(uri.host, uri.port)
-    request = Net::HTTP::Post.new("/tasks")
-    request.add_field('Content-Type', 'application/json')
-    request.body = {"task" => {"name" => "#{name}", "description" => "#{description}"}}.to_json
-    response = http.request(request)
-    say "# OK" if response.code == "201"
+    params = { task: { name: name, description: description } }
+    response = HTTParty.post "#{ ENV['LOCALHOST_URL'] }", headers: {'Content-Type' => 'application/json'}, body: params.to_json
+    say "# OK" if response.code == 201
   end
 end
 
 command :all do |c|
   c.syntax = 'all [options]'
   c.description = 'List all the tasks'
-  c.action do |options|
-    uri = URI('http://localhost:9393/tasks/all')
-    res = Net::HTTP.get(uri)
-    say "#{ res }"
+  c.action do
+    response = HTTParty.get "#{ ENV['LOCALHOST_URL'] }/all"
+    say "#{ response }"
   end
 end
 
 command :todo do |c|
   c.syntax = '[options]'
   c.description = 'List all the todo tasks'
-  c.action do |options|
-    res = Net::HTTP.get(URI('http://localhost:9393/tasks'))
-    say "#{ res }"
+  c.action do
+    response = HTTParty.get "#{ ENV['LOCALHOST_URL'] }"
+    say "#{ response }"
   end
 end
-
-default_command :todo
