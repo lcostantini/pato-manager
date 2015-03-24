2f9bc7e9c3916badcf987f32e83a720f7b4c0b55
lcostantini <costantinileandro1@gmail.com>
2015-03-24 18:14:47 -0300

    Show category for a task.

--- models/response_decorator.rb
+++ models/response_decorator.rb
@@ -1,19 +1,12 @@
 class ResponseDecorator
-  def header_task header
-    if header.nil?
-      ['Name', 'Description', 'Date', 'State']
-    else
-      header[:header]
-    end
+  def decorate_table response
+    Terminal::Table.new rows: response_row(response),
+      headings: ['Name', 'Description', 'Date', 'State', 'Category']
   end
 
-  def decorate_table response, header = nil
-    Terminal::Table.new rows: response_row(response), headings: header_task(header)
-  end
-
-  def response_row(response)
+  def response_row response
     response.map do |r|
-      ["#{ r["name"] }", "#{ r["description"] }", "#{ r["created_at"] }", "#{ r["state"] }"]
+      ["#{ r["name"] }", "#{ r["description"] }", "#{ r["created_at"] }", "#{ r["state"] }", "#{ r["category"] }"]
     end
   end
 end
