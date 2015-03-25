ef713cce72f87897cd8528a97181a0b5255869c6
lcostantini <costantinileandro1@gmail.com>
2015-03-25 14:46:12 -0300

    Add number column.

--- models/response_decorator.rb
+++ models/response_decorator.rb
@@ -1,12 +1,12 @@
 class ResponseDecorator
   def decorate_table response
     Terminal::Table.new rows: response_row(response),
-      headings: ['Name', 'Description', 'Date', 'State', 'Category']
+      headings: ['Number', 'Name', 'Description', 'Date', 'State', 'Category']
   end
 
   def response_row response
     response.map do |r|
-      ["#{ r["name"] }", "#{ r["description"] }", "#{ r["created_at"] }", "#{ r["state"] }", "#{ r["category"] }"]
+      ["#{ r["id"] }", "#{ r["name"] }", "#{ r["description"] }", "#{ r["created_at"] }", "#{ r["state"] }", "#{ r["category"] }"]
     end
   end
 end
