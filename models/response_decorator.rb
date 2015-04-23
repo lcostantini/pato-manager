4fe912a1a99ead161c6afa14a0c16daeacdfc441
lcostantini <costantinileandro1@gmail.com>
2015-04-23 19:52:14 -0300

    Add options to show todo list.

--- models/response_decorator.rb
+++ models/response_decorator.rb
@@ -1,12 +1,44 @@
 class ResponseDecorator
-  def decorate_table response
-    Terminal::Table.new rows: response_row(response),
-      headings: ['Number', 'Name', 'Description', 'Date', 'State', 'Category']
+  def decorate_table response, options = {}
+    @response = response
+    @options = options
+    return '# The list is empty' if response.empty?
+    Terminal::Table.new headings: get_head, rows: get_rows
   end
 
-  def response_row response
-    response.map do |r|
-      ["#{ r["id"] }", "#{ r["name"] }", "#{ r["description"] }", "#{ r["created_at"] }", "#{ r["state"] }", "#{ r["category"] }"]
+  private
+
+  def default_rows
+    @response.map do |r|
+      ["#{ r['id'] }",
+       "#{ r['name'] }",
+       "#{ r['state'] }",
+       "#{ r['category'] }"]
     end
+  end
+
+  def get_head
+    head = ['Id', 'Name', 'State', 'Category']
+    head << 'Date' if @options[:date]
+    head << 'Description' if @options[:description]
+    head
+  end
+
+  def get_rows
+    rows = default_rows
+    return rows if @options.empty?
+    new_rows = options_rows
+    (0...rows.count).map { |i| (rows[i] << new_rows[i]).flatten }
+  end
+
+  def options_rows
+    date = option_response 'created_at' if @options[:date]
+    description = option_response 'description' if @options[:description]
+    return ([date] + [description]).compact.first if @options.count == 1
+    [date, description].transpose
+  end
+
+  def option_response option
+    @response.map { |r| "#{ r[option] }" }
   end
 end
