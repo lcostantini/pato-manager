f4d7f56f0dc50f4ba285d8ff550e5b95c2a0983f
lcostantini <costantinileandro1@gmail.com>
2015-08-24 14:13:12 -0300

    Update script.

--- pato-manager.sh
+++ pato-manager.sh
@@ -1,63 +1,51 @@
 #!/bin/bash
 
-#check_file_exists() {
-#  if [ -e "$HOME/.pato-manager" ]
-#  then
-#    echo "File already exists."
-#  else
-#    touch $HOME/.pato-manager
-#    echo "File was created."
-#  fi
-#}
-
-ask_to_get_token() {
-readme
-while true; do
-  read -p "Get the token from GitHub? (y/n): " yn
-  case $yn in
-    [Yy]* ) get_token; break;;
-    [Nn]* ) manually; exit;;
-    * ) echo "Please answer yes or no.";;
-  esac
-done
+save_token() {
+token=$(create_token)
+#if [ token == 1 ]
+#then
+#  echo 'exit code is 1'
+#fi
 }
 
-get_token() {
+create_token() {
 read -p "Please enter your GitHub username: " username
 curl "https://api.github.com/authorizations" -u $username --data '{"note": "admin script"}' |
-  ruby -r 'json' -e "puts JSON.parse(STDIN.read)['token']" > $HOME/.pato-manager
+  ruby -r 'json' -e "
+  response = JSON.parse(STDIN.read)
+  puts ''
+  if response['token']
+    puts response['token']
+  else
+    raise response.to_s
+  end" || (printf "\n# There is an error getting the token, please try again or find one manually.\n" && manually)
 }
 
 manually() {
-echo "
-# To get the token manually, run this command
-
+echo "\n
+\e[0;34mMANUALLY:\e[0m\n
+To get the token manually, run the commands at the bottom.
+In the curl command replacing the USERNAME for your GitHub username,
+then execute the second command that creates a hidden file called pato-manager.
+Finally copy the token provided by the Github API nd save it in the file.\n
 curl "https://api.github.com/authorizations" -u USERNAME --data '{"note": "admin script"}'
-
-# replacing the USERNAME for your GitHub username,
-# then copy the token and save it in a hidden file called pato-manager.
-# The file needs to be in the home directory. ie: $HOME/.pato-manager."
+touch ~/.pato-manager"
 }
 
 readme() {
-echo "
-# HOW IT WORKS:
-# To know the user that is using the app, Pato Manager
-# needs to know your GitHub email, for that, the script ask for
-# your GitHub username and try to create a public token
-# (only reads your public profile details).
-
-# The GitHub API ask for your password, then script capture the
-# response from the API and save in pato-manager file.
-
-# You can see your token in $HOME/.pato-manager
-# and in GitHub https://github.com/settings/tokens.
-
-***
-# If you want to get the token manually, respond 'no' to the
-# question and follow the instructions.
+echo -e "\n
+\e[0;34mHOW IT WORKS:\e[0m\n
+To know the user that is using the app or create a new one,
+Pato Manager needs to know your GitHub email[*], for that,
+the script ask your GitHub username and try to create or read
+a public token .- \e[0;31m**only gets your public profile details**\e[0m -.\n
+The GitHub API ask for your password, then the script capture
+the response from the API and save in pato-manager file.
+You can see your token in $HOME/.pato-manager
+and in GitHub https://github.com/settings/tokens.\n
+[*]: Because it's easier than having to remember another login account.\n
 "
 }
 
-#check_file_exists
-ask_to_get_token
+readme
+save_token
