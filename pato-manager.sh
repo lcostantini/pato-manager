3d56756eb129bdf8483341d0187039017f0e58bf
lcostantini <costantinileandro1@gmail.com>
2015-08-25 15:05:48 -0300

    Add explicit scopes to create a token.

--- pato-manager.sh
+++ pato-manager.sh
@@ -2,6 +2,7 @@
 
 save_token() {
 token=$(create_token)
+echo $token
 #if [ token == 1 ]
 #then
 #  echo 'exit code is 1'
@@ -10,25 +11,26 @@ token=$(create_token)
 
 create_token() {
 read -p "Please enter your GitHub username: " username
-curl "https://api.github.com/authorizations" -u $username --data '{"note": "admin script"}' |
+curl "https://api.github.com/authorizations" -u $username --data '{"scopes": ["user:email"], "note": "pato-manger"}' |
   ruby -r 'json' -e "
   response = JSON.parse(STDIN.read)
-  puts ''
   if response['token']
     puts response['token']
   else
     raise response.to_s
-  end" || (printf "\n# There is an error getting the token, please try again or find one manually.\n" && manually)
+  end" || (manually)
 }
 
 manually() {
-echo "\n
+echo -e "\n
+There is an error getting the token, please read the error message and try again
+or find a token manually.\n\n
 \e[0;34mMANUALLY:\e[0m\n
 To get the token manually, run the commands at the bottom.
 In the curl command replacing the USERNAME for your GitHub username,
 then execute the second command that creates a hidden file called pato-manager.
 Finally copy the token provided by the Github API nd save it in the file.\n
-curl "https://api.github.com/authorizations" -u USERNAME --data '{"note": "admin script"}'
+curl "https://api.github.com/authorizations" -u USERNAME --data '{"scopes": ["user:email"], "note": "pato-manager"}'
 touch ~/.pato-manager"
 }
 
@@ -43,8 +45,7 @@ The GitHub API ask for your password, then the script capture
 the response from the API and save in pato-manager file.
 You can see your token in $HOME/.pato-manager
 and in GitHub https://github.com/settings/tokens.\n
-[*]: Because it's easier than having to remember another login account.\n
-"
+[*]: Because it's easier than having to remember another login account.\n"
 }
 
 readme
